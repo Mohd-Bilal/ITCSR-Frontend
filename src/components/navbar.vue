2d3efebdb4834793bb8e3023291429840e2357e2
Bilal <bilaljaleel123@gmail.com>
2018-10-24 21:39:45 +0530

    Added skeletal form

---
+++ src/components/navbar.vue
@@ -0,0 +1,36 @@
+<template>
+ <ul>
+  <li><a href="#">Home</a></li>
+  <li><a href="#">About</a></li>
+
+</ul>     
+</template>
+
+
+
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color:gray;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+/* li a:hover {
+    background-color: #;
+} */
+</style>
---
+++ src/views/dashboard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+    <navbar></navbar>
+    <label>Input</label>
+   
+            <input v-model="input" type="text">
+            <input v-model="input" type="text">
+            <input v-model="input" type="text">
+        
+        
+            <button @click="test">Hello</button>
+        
+    </div>
+</template>
+<style>
+</style>
+<script>
+import navbar from "../components/navbar.vue";
+var input;
+export default {
+  components: {
+    navbar
+  },
+  data() {
+    return {
+      input
+    };
+  },
+  methods: {
+    test: function() {
+      console.log(this.input);
+    }
+  }
+};
+</script>
