6ad048033d45d1b71d7644cf79aafa6182f1cbfb
Bilal <bilaljaleel123@gmail.com>
2018-10-25 01:17:29 +0530

    Added form

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -2,14 +2,21 @@
     <div>
     <navbar></navbar>
     <div class="form">
-    <label>Input</label><br>
-   
-            <input v-model="input" type="text"><br>
-            <input v-model="input" type="text"><br>
-            <input v-model="input" type="text"><br>
-        
-        
-            <button @click="test" class="but">Hello</button>
+    <label>Project Id</label><br>
+    <input v-model="projectId" type="text"><br>
+    <label>File Number</label><br>
+    <input v-model="fileNumber" type="text"><br>
+    <label>Principal Investigator Id</label><br>
+    <input v-model="principalInvestigatorId" type="text"><br>
+    <label>Project Name</label><br>
+    <input v-model="projectName" type="text"><br>
+    <label>Data</label><br>
+    <input v-model="data" type="text"><br>
+    <label>Start Date</label><br>
+    <input v-model="startDate" type="text"><br>
+    <label>Duration</label><br>
+    <input v-model="duration" type="text"><br>
+    <button @click="test" class="but">Hello</button>
         
     </div>
     </div>
@@ -56,19 +63,37 @@ label{
 </style>
 <script>
 import navbar from "../components/navbar.vue";
-var input;
+var projectId,fileNumber,principalInvestigatorId,projectName,data,startDate,duration;
 export default {
   components: {
     navbar
   },
   data() {
     return {
-      input
+      projectId,fileNumber,principalInvestigatorId,projectName,data,startDate,duration
     };
   },
   methods: {
     test: function() {
-      console.log(this.input);
+      var request = {}
+      request.project_id = this.projectId
+      request.file_no = this.fileNumber
+      request.principal_investigator_id = this.principalInvestigatorId
+      request.name = this.projectName
+      request.data = {}
+      request.data.inp = this.data
+      request.start_data = this.startDate
+      request.duration = this.duration
+      window.axios({url:'http://localhost:3000/proposal/create',method:'POST',data:request})
+      .then(function(result){
+        if(result.data.success)
+          console.log("Success")
+        else
+          console.log(result.data.error)  
+      }).catch(function(err){
+        console.log(err)
+      });
+
     }
   }
 };
