709bf18bb3793bcd97e3ceac5c061017ac632646
nandukmg <nandukrishnan55555@gmail.com>
2018-11-01 11:02:40 +0530

    addHead complete

--- src/views/addHead.vue
+++ src/views/addHead.vue
@@ -1,7 +1,9 @@
 <template>
 <div id="central">
-  <!-- <dropdown :options="arrayOfObjects" :selected="object" v-on:updateOption="methodToRunOnSelect"></dropdown> -->
-<select @click="fetchAllHeads" v-model="selected"  >
+<div id="dropdownDiv">
+  <h1>Add Heads</h1>
+  <hr>
+  <h3>Head:</h3><select @click="fetchAllHeads" v-model="selected"  >
   <option disabled value="">Please select one</option>
   <option @click="methodToRunOnSelect(head.head_id)" v-for="head in heads" :key="head.head_id" >{{head.name}}</option>
 
@@ -10,28 +12,48 @@
 </select>
 <input v-if="seen===true" v-model="fund" placeholder="Fund"/>
 <button v-if="seen===true" v-on:click="addhead">Add</button>
-
+</div>
+<ul class="heads">
+  <li v-for="added_head in added_heads" >{{added_head.head}} {{added_head.fund}}</li>
+</ul>
 </div>
 </template>
 
 <style>
-ul.reset,
-ul.reset li,
-ul.reset ul li {
-  margin: 0;
-  padding: 0;
-  text-indent: 0;
-  list-style-type: 0;
-  display: block;
+#dropdownDiv{
+  margin-top:10%;
+
 }
+
+#dropdownDiv select{
+  margin-left:1vw;
+}
+#dropdownDiv h1{
+  text-align: left;
+}
+
+#dropdownDiv h3{
+  display:inline-block;
+  margin-left: 7vw;
+}
+.heads{
+  padding-left:8vw;
+  text-align: left;
+}
+
+.heads li{
+  font-size:20px;
+}
+
 #central {
-  text-align: center;
+  padding-left: 20%;
+  padding-right: 20%;
   margin: 0 auto;
 }
 </style>
 <script>
 //import dropdown from 'vue-dropdowns';l
-var selected,fund,selected_id,heads;
+var selected,fund,selected_id,heads,added_heads=[];
 export default {
   data() {
     return {
@@ -39,7 +61,8 @@ export default {
       seen: false,
       selected,
       selected_id,
-      heads
+      heads,
+      added_heads
       // heads: [{id:1,name:"Mach"},{id:2,name:"Trave"},{id:3,name:"manpowe"}]
     };
   },
@@ -83,8 +106,8 @@ export default {
       var self = this;
       if (this.validate()) {
         var request = {};
-        // request.project_id = null;
-        request.project_id = self.$store.state.project_id;
+        request.project_id = 10;
+        // request.project_id = self.$store.state.project_id;
         request.head_id = this.selected_id;
         request.fund = this.fund;
         console.log(request);
@@ -95,7 +118,14 @@ export default {
             data: request
           })
           .then(function(result) {
-            if (result.data.success) console.log("Success");
+            if (result.data.success) {
+              console.log("Success");
+              console.log(self.selected);
+              console.log(self.fund)
+              self.added_heads.push({"head":self.selected,"fund":self.fund})
+              console.log(self.added_heads[0].head);
+              console.log("evide");
+            }
             else console.log(result);
           })
           .catch(function(err) {
